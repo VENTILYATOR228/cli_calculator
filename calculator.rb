db7c2729b8d5b7a13afe58de92c4cc08bdd81e52
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-11-08 23:37:24 +0300

    Implementing the calculator through eval method

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,4 @@
-input = gets.chomp
-arguments = input.scan(/\S+/)
-sign = nil
-result = arguments.reduce do |result, item|
-  case item
-  when "+"
-    sign = "+"
-  when "-"
-    sign = "-"
-  else
-    if sign == "+"
-      result = result.to_i + item.to_i
-    else
-      result = result.to_i - item.to_i
-    end
-  end
-  result
-end
-
+input = ARGV
+problem = input.reduce { |problem, item| problem + " " + item }
+result = eval(problem)
 puts result
