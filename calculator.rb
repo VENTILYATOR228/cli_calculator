e33cc4bdc8c8369ca6ccb7519916a30573bbe6a0
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-11-10 11:40:02 +0300

    Delete unnecessary variables

--- calculator.rb
+++ calculator.rb
@@ -1,24 +1,24 @@
 def split_string_in_array_to_array(input)
   string = input.reduce { |string, item| resut + item }
-  arguments = string.scan(/\S+/)
+  string.scan(/\S+/)
 end
 
 def split_string_to_array(input)
-  arguments = input.scan(/\S+/)
+  input.scan(/\S+/)
 end
 
-def array_or_string(input)
+def split_array_or_string(input)
   case input
   when Array
-    arguments = split_string_in_array_to_array(input)
+    split_string_in_array_to_array(input)
   when String
-    arguments = split_string_to_array(input)
+    split_string_to_array(input)
   end
 end
 
 def eval_math(input)
   sign = nil
-  arguments = array_or_string(input)
+  arguments = split_array_or_string(input)
   result = arguments.reduce do |result, item|
     result = result.to_i
     if item != "+" && item != "-"
